1666bfc2ad4d2d5a69a6606c7aea3dcbd30eb5de
Noam2Stein <noamsteindev@gmail.com>
2025-08-07 09:10:02 +0300

    method tests

---
+++ repetitive/tests/methods.rs
@@ -0,0 +1,11 @@
+use repetitive::repetitive;
+
+repetitive! {
+    const _: () = assert!(@(1.neg() == -1) == true);
+    const _: () = assert!(@(1.add(4) == 5) == true);
+
+    const _: () = assert!(@([1, 2, 3].index(1) == 2) == true);
+    const _: () = assert!(@([1, 2, 3].enumerate() == [[0, 1], [1, 2], [2, 3]]) == true);
+    const _: () = assert!(@([1, 2, 3].zip([4, 5, 6]) == [[1, 4], [2, 5], [3, 6]]) == true);
+    const _: () = assert!(@([1, 2, 3].chain([4, 5, 6]) == [1, 2, 3, 4, 5, 6]) == true);
+}
