c946026e884dc50e7624bf54ab8c12eb7330e169
Noam2Stein <noamsteindev@gmail.com>
2025-08-12 22:45:09 +0300

    update swizzle example
    added doc

--- examples/swizzle/src/main.rs
+++ examples/swizzle/src/main.rs
@@ -13,19 +13,33 @@ fn main() {
 }
 
 repetitive! {
-    @for N in [2, 3, 4] {
+    @for N in 2..=4 {
         @let VecN = @['Vec N];
         @let components = ['x, 'y, 'z, 'w][0..N];
 
-        #[derive(Debug, Clone, Copy)]
-        struct @VecN {
+        @let component_list = match N {
+            2 => "`x` and `y`",
+            3 => "`x`, `y` and `z`",
+            4 => "`x`, `y`, `z` and `w`",
+        };
+
+        @let vector_fields = @{
             @for c in components {
                 @c: f32,
             }
+        };
+
+        #[doc = @str["A vector with " N " components (" component_list ")"]]
+        #[derive(Debug, Clone, Copy)]
+        struct @VecN {
+            @vector_fields
         }
 
         impl @VecN {
             @for x in components, y in components {
+                @let component_list = @str["`" x "` and `" y "`"];
+
+                #[doc = @str["Returns a vector with the " component_list " components of this vector."]]
                 fn @[x y](self) -> Vec2 {
                     Vec2 {
                         x: self.@x,
@@ -35,6 +49,9 @@ repetitive! {
             }
 
             @for x in components, y in components, z in components {
+                @let component_list = @str["`" x "`, `" y "` and `" z "`"];
+
+                #[doc = @str["Returns a vector with the " component_list " components of this vector."]]
                 fn @[x y z](self) -> Vec3 {
                     Vec3 {
                         x: self.@x,
@@ -45,6 +62,9 @@ repetitive! {
             }
 
             @for x in components, y in components, z in components, w in components {
+                @let component_list = @str["`" x "`, `" y "`, `" z "` and `" w "`"];
+
+                #[doc = @str["Returns a vector with the " component_list " components of this vector."]]
                 fn @[x y z w](self) -> Vec4 {
                     Vec4 {
                         x: self.@x,
