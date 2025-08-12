600443f3225103264a2b7fa54c1b4c0c53d70e03
Noam2Stein <noamsteindev@gmail.com>
2025-08-12 23:10:16 +0300

    Update concat.rs

--- tests/concat.rs
+++ tests/concat.rs
@@ -3,7 +3,7 @@ use repetitive::repetitive;
 repetitive! {
     const _: () = assert!(@(@["Hello" "World"] == 'HelloWorld));
     const _: () = assert!(@(@['Hello "World"] == 'HelloWorld));
-    const _: () = assert!(@(@["Hello" 'Worl 'd] == 'HelloWorld));
+    const _: () = assert!(@(@["Hello" '_ 'Worl 'd] == 'Hello_World));
 
     const _: () = assert!(@(@str["Hello" "World"] == "HelloWorld"));
     const _: () = assert!(@(@str['Hello "World"] == "HelloWorld"));
