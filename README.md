522cc53dab65819c33af8c9876ff89b1db87ab9b
Noam2Stein <noamsteindev@proton.me>
2026-07-27 18:52:04 +0300

    Update crate documentation (sync to book)

--- README.md
+++ README.md
@@ -3,15 +3,19 @@
 A metaprogramming macro with control flow syntax.
 
 Rust declarative macros are great for most metaprogramming, and result in clean
-and readable code. However in some rare cases, code requires repetition logic
+and readable code. However, in some rare cases, code requires repetition logic
 that is too complicated for declarative macros.
 
-The [`repetitive`] macro solves this by introducing metaprogramming control flow
+The `repetitive` macro solves this by introducing metaprogramming control flow
 constructs, like [`$for`] for repetition, [`$if`] for conditions, and [`$let`]
 for reusing values, as well as identifier concatenation and other useful
 features.
 
-For a complete list of features, see the [macro documentation].
+Think of `repetitive` as not trying to *replace* declarative macros, but as a
+complementary tool handling repetition logic. This is basically a combination of
+[`paste`] and [`seq-macro`] with improved ergonomics.
+
+For detailed documentation, see the [repetitive Documentation].
 
 ## Example
 
@@ -41,12 +45,11 @@ functions).
 
 Instead, to make the code as readable as possible, we will keep the function
 definitions in declarative macros, then invoke those macros from a
-[`repetitive`] block that handles the repetition logic.
+`repetitive` block that handles the repetition logic.
 
 ```rust
-macro_rules! define_swizzle_fn {
+macro_rules! define_swizzle_function {
     ($f:ident, $x:ident, $y:ident, $z:ident) => {
-        #[inline]
         pub fn $f(self) -> Vec3 {
             Vec3 {
                 x: self.$x,
@@ -57,13 +60,12 @@ macro_rules! define_swizzle_fn {
     };
 }
 
-macro_rules! define_set_swizzle_fn {
+macro_rules! define_set_swizzle_function {
     ($f:ident, $x:ident, $y:ident, $z:ident) => {
-        #[inline]
-        pub fn $f(&mut self, value: Vec3) {
-            self.$x = value.x;
-            self.$y = value.y;
-            self.$z = value.z;
+        pub fn $f(&mut self, other: Vec3) {
+            self.$x = other.x;
+            self.$y = other.y;
+            self.$z = other.z;
         }
     };
 }
@@ -72,24 +74,20 @@ repetitive! {
     $let elements = ["x", "y", "z"];
 
     impl Vec3 {
-        $for x in elements {
-            $for y in elements {
-                $for z in elements {
-                    define_swizzle_fn!(
-                        $(format!("{x}{y}{z}")),
-                        $x,
-                        $y,
-                        $z
-                    );
-                    $if x != y && x != z && y != z {
-                        define_set_swizzle_fn!(
-                            $(format!("set_{x}{y}{z}")),
-                            $x,
-                            $y,
-                            $z
-                        );
-                    }
-                }
+        $for (x, y, z) in iproduct!(elements, elements, elements) {
+            define_swizzle_function!(
+                $(format!("{x}{y}{z}")),
+                $x,
+                $y,
+                $z
+            );
+            $if x != y && x != z && y != z {
+                define_set_swizzle_function!(
+                    $(format!("set_{x}{y}{z}")),
+                    $x,
+                    $y,
+                    $z
+                );
             }
         }
     }
@@ -98,17 +96,17 @@ repetitive! {
 
 ## Compile times and internal architecture
 
-Internally, this macro uses a small interpreter that verifies and expands the
+Internally, `repetitive` uses a small interpreter that verifies and expands the
 code. Unlike [`crabtime`] and similar crates, no external tool is invoked.
 
 Keeping this interpreter small and fast to compile is a high-priority goal.
-Depending on the size of your crate, [`repetitive`] may be either insignificant
+Depending on the size of your crate, `repetitive` may be either insignificant
 or too heavy in terms of compile times.
 
 For example, the [`ggmath`] crate defines swizzle functions similar to the
-example above, but avoids defining them using [`repetitive`] in order to keep
+example above, but avoids defining them using `repetitive` in order to keep
 compile times as short as possible. Instead, it manually invokes all swizzle
-combinations, then uses [`repetitive`] in tests to make sure the manual code
+combinations, then uses `repetitive` in tests to make sure the manual code
 is correct.
 
 ## License
@@ -125,11 +123,12 @@ Unless you explicitly state otherwise, any contribution intentionally submitted
 for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
 dual licensed as above, without any additional terms or conditions.
 
-[`repetitive`]: https://docs.rs/repetitive/latest/repetitive/macro.repetitive.html
-[`$for`]: https://docs.rs/repetitive/latest/repetitive/macro.repetitive.html#for
-[`$if`]: https://docs.rs/repetitive/latest/repetitive/macro.repetitive.html#if
-[`$let`]: https://docs.rs/repetitive/latest/repetitive/macro.repetitive.html#let
-[macro documentation]: https://docs.rs/repetitive/latest/repetitive/macro.repetitive.html
+[`$for`]: TODO
+[`$if`]: TODO
+[`$let`]: TODO
+[`paste`]: https://crates.io/crates/paste
+[`seq-macro`]: https://crates.io/crates/seq-macro
+[repetitive Documentation]: TODO
 [deep nesting]: https://www.youtube.com/watch?v=CFRhGnuXG-4
 [`crabtime`]: https://crates.io/crates/crabtime
 [`ggmath`]: https://crates.io/crates/ggmath
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,25 @@
-//! This crate provides a metaprogramming macro with control flow syntax.
+//! A metaprogramming macro with control flow syntax.
 //!
-//! Rust declarative macros are great for most metaprogramming, and result in
-//! clean and readable code. However in some rare cases, code requires
-//! repetition logic that is too complicated for declarative macros.
+//! Rust declarative macros are great for most metaprogramming, and result in clean
+//! and readable code. However, in some rare cases, code requires repetition logic
+//! that is too complicated for declarative macros.
 //!
-//! The [`repetitive`] macro solves this by introducing metaprogramming control
-//! flow constructs, like [`$for`] for repetition, [`$if`] for conditions, and
-//! [`$let`] for reusing values, as well as identifier concatenation and other
-//! useful features.
+//! The `repetitive` macro solves this by introducing metaprogramming control flow
+//! constructs, like [`$for`] for repetition, [`$if`] for conditions, and [`$let`]
+//! for reusing values, as well as identifier concatenation and other useful
+//! features.
 //!
-//! For a complete list of features, see the [repetitive Documentation].
+//! Think of `repetitive` as not trying to *replace* declarative macros, but as a
+//! complementary tool handling repetition logic. This is basically a combination of
+//! [`paste`] and [`seq-macro`] with improved ergonomics.
+//!
+//! For detailed documentation, see the [repetitive Documentation].
 //!
 //! # Example
 //!
-//! This example shows the intended usage of the macro. It is not recommended to
-//! use this macro to fully replace declarative macros, because that would
-//! probably result in messy unreadable code with ugly [deep nesting].
+//! This example shows the intended usage of the macro. It is not recommended to use
+//! this macro to fully replace declarative macros, because that would probably
+//! result in messy unreadable code with ugly [deep nesting].
 //!
 //! Let's say we have a vector3 type:
 //!
@@ -29,22 +33,21 @@
 //! ```
 //!
 //! Our goal is to define swizzle functions (for example `vec3.zxy()` and
-//! `vec3.xxy()`), and functions that set certain swizzle combinations (for
-//! example `vec3.set_xzy(another_vec3)`, but not `vec3.set_xxy(another_vec3)`
-//! because that does not make sense).
+//! `vec3.xxy()`), and functions that set certain swizzle combinations (for example
+//! `vec3.set_xzy(another_vec3)`, but not `vec3.set_xxy(another_vec3)` because that
+//! does not make sense).
 //!
-//! Doing this using only declarative macros would result in unreadable code, as
-//! it requires multiple layers of repetition and conditional logic (for
-//! `set_...` functions).
+//! Doing this using only declarative macros would result in unreadable code, as it
+//! requires multiple layers of repetition and conditional logic (for `set_...`
+//! functions).
 //!
 //! Instead, to make the code as readable as possible, we will keep the function
 //! definitions in declarative macros, then invoke those macros from a
-//! [`repetitive`] block that handles the repetition logic.
+//! `repetitive` block that handles the repetition logic.
 //!
 //! ```rust
-//! macro_rules! define_swizzle_fn {
+//! macro_rules! define_swizzle_function {
 //!     ($f:ident, $x:ident, $y:ident, $z:ident) => {
-//!         #[inline]
 //!         pub fn $f(self) -> Vec3 {
 //!             Vec3 {
 //!                 x: self.$x,
@@ -55,13 +58,12 @@
 //!     };
 //! }
 //!
-//! macro_rules! define_set_swizzle_fn {
+//! macro_rules! define_set_swizzle_function {
 //!     ($f:ident, $x:ident, $y:ident, $z:ident) => {
-//!         #[inline]
-//!         pub fn $f(&mut self, value: Vec3) {
-//!             self.$x = value.x;
-//!             self.$y = value.y;
-//!             self.$z = value.z;
+//!         pub fn $f(&mut self, other: Vec3) {
+//!             self.$x = other.x;
+//!             self.$y = other.y;
+//!             self.$z = other.z;
 //!         }
 //!     };
 //! }
@@ -70,24 +72,20 @@
 //!     $let elements = ["x", "y", "z"];
 //!
 //!     impl Vec3 {
-//!         $for x in elements {
-//!             $for y in elements {
-//!                 $for z in elements {
-//!                     define_swizzle_fn!(
-//!                         $(format!("{x}{y}{z}")),
-//!                         $x,
-//!                         $y,
-//!                         $z
-//!                     );
-//!                     $if x != y && x != z && y != z {
-//!                         define_set_swizzle_fn!(
-//!                             $(format!("set_{x}{y}{z}")),
-//!                             $x,
-//!                             $y,
-//!                             $z
-//!                         );
-//!                     }
-//!                 }
+//!         $for (x, y, z) in iproduct!(elements, elements, elements) {
+//!             define_swizzle_function!(
+//!                 $(format!("{x}{y}{z}")),
+//!                 $x,
+//!                 $y,
+//!                 $z
+//!             );
+//!             $if x != y && x != z && y != z {
+//!                 define_set_swizzle_function!(
+//!                     $(format!("set_{x}{y}{z}")),
+//!                     $x,
+//!                     $y,
+//!                     $z
+//!                 );
 //!             }
 //!         }
 //!     }
@@ -96,24 +94,25 @@
 //!
 //! # Compile times and internal architecture
 //!
-//! Internally, this macro uses a small interpreter that verifies and expands
-//! the code. Unlike [`crabtime`] and similar crates, no external tool is
-//! invoked.
+//! Internally, `repetitive` uses a small interpreter that verifies and expands the
+//! code. Unlike [`crabtime`] and similar crates, no external tool is invoked.
 //!
 //! Keeping this interpreter small and fast to compile is a high-priority goal.
-//! Depending on the size of your crate, [`repetitive`] may be either
-//! insignificant or too heavy in terms of compile times.
+//! Depending on the size of your crate, `repetitive` may be either insignificant
+//! or too heavy in terms of compile times.
 //!
 //! For example, the [`ggmath`] crate defines swizzle functions similar to the
-//! example above, but avoids defining them using [`repetitive`] in order to
-//! keep compile times as short as possible. Instead, it manually invokes all
-//! swizzle combinations, then uses [`repetitive`] in tests to make sure the
-//! manual code is correct.
+//! example above, but avoids defining them using `repetitive` in order to keep
+//! compile times as short as possible. Instead, it manually invokes all swizzle
+//! combinations, then uses `repetitive` in tests to make sure the manual code
+//! is correct.
 //!
-//! [`$for`]: repetitive#for
-//! [`$if`]: repetitive#if
-//! [`$let`]: repetitive#let
-//! [repetitive Documentation]: file:///home/user/dev/repetitive/docs/book/metaprogramming-keywords.html
+//! [`$for`]: TODO
+//! [`$if`]: TODO
+//! [`$let`]: TODO
+//! [`paste`]: https://crates.io/crates/paste
+//! [`seq-macro`]: https://crates.io/crates/seq-macro
+//! [repetitive Documentation]: TODO
 //! [deep nesting]: https://www.youtube.com/watch?v=CFRhGnuXG-4
 //! [`crabtime`]: https://crates.io/crates/crabtime
 //! [`ggmath`]: https://crates.io/crates/ggmath
@@ -124,9 +123,9 @@ use proc_macro::TokenStream;
 
 /// A metaprogramming macro with control flow syntax.
 ///
-/// See the [repetitive Documentation] for more information.
+/// For detailed documentation, see the [repetitive Documentation].
 ///
-/// [repetitive Documentation]: file:///home/user/dev/repetitive/docs/book/metaprogramming-keywords.html
+/// [repetitive Documentation]: TODO
 #[proc_macro]
 pub fn repetitive(input: TokenStream) -> TokenStream {
     let _ = input;
